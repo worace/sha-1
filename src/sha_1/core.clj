4b5c9b19f42eb8199b89bd304dbae1e45a878562
Horace Williams <horace.d.williams@gmail.com>
2015-10-26 20:02:23 -0600

    finishing up message prep
    - added separator bit to distinguish message
    from added padding

--- src/sha_1/core.clj
+++ src/sha_1/core.clj
@@ -1,10 +1,11 @@
-(ns sha-1.core)
+(ns sha-1.core
+  (:require [clojure.string :refer [join]]))
 
 (defn str->codes [s]
   (map int s))
 
 (defn bstring [i]
-  (Integer/toBinaryString i))
+  (Integer/toBinaryString (bit-and i 0xFF)))
 
 (defn padded [bit-count i]
   (let [s (bstring i)]
@@ -28,26 +29,31 @@
            (congruence-bits msg-bits)
            (padded 64 (count msg)))))
 
+;; Byte Arrays
+
 (defn msg->bytes [msg]
   (byte-array (map (comp byte int) msg)))
 
-(def block-bit-length 512)
-(def block-byte-length (/ 8 block-bit-length))
-(def message-length-bits 64)
-(def message-length-bytes (/ 8 64))
-
-(defn padding-bytes [msg-bytes]
+(defn padding-bytes
+  "Add appropriate number of 0 bytes to bring message into congruence with 448
+   mod 512 (56 mod 64 bytes). Additionally, the first bit of this padding sequence
+   should be set 1 to separate original message from padding."
+  [msg-bytes]
   (let [mod-offset (- 56 (mod (count msg-bytes) 64))
         bytes-needed (if (< mod-offset 0)
                        (+ mod-offset 64)
                        mod-offset)]
     (byte-array
-     (take bytes-needed (repeat 0)))))
+     (concat
+      [0x80]
+      (take (dec bytes-needed) (repeat 0))))))
 
 (defn long-empty-bytes-count [n]
   (int (/ (Long/numberOfLeadingZeros n) 8)))
 
-(defn long->bytes [n]
+(defn long->bytes
+  "Chunks 64-bit long into appropriate number of bytes"
+  [n]
   (loop [n n
          bytes (list)]
     (if (= 0 n)
@@ -59,3 +65,24 @@
         empty-bytes (take pad-count (repeat 0))
         filled-bytes (long->bytes (count msg))]
     (byte-array (concat empty-bytes filled-bytes))))
+
+(defn inspect-byte-array
+  "Util function for manually inspecting byte arrays as strings"
+  [barray]
+  (->> barray
+       (map (partial padded 8))
+       (join " ")))
+
+(defn with-padding-bytes [m-bytes] (byte-array (concat m-bytes (padding-bytes m-bytes))))
+(defn with-message-length-bytes [m-bytes] (byte-array (concat m-bytes (msg-length-bytes m-bytes))))
+
+(defn msg->prepped-barray
+  "takes incoming string message through various steps needed to
+   convert it to a mod-512 byte array with appropriate padding and
+   meessage length appended"
+  [msg]
+  (-> msg
+      (msg->bytes)
+      (concat (padding-bytes msg))
+      (concat (msg-length-bytes msg))
+      (byte-array)))
--- test/sha_1/core_test.clj
+++ test/sha_1/core_test.clj
@@ -35,24 +35,50 @@
   (is (= (count prepped-ab) (count (prep-message "ab"))))
   (is (= prepped-ab (prep-message "ab"))))
 
+;; byte arrays
+
 (deftest test-msg-bytes
   (is (= 2 (count (msg->bytes "ab")))))
 
 (deftest test-padding-length
-  (is (= 54 (count (padding-bytes (msg->bytes "ab"))))))
+  (testing "pads short message to 56 total bytes, leaving 8 bytes in
+            same 512-bit block for message length. Additionally, leading
+            padding bit should be 1 to distinguish padding from message"
+    (is (= 54 (count (padding-bytes "ab"))))
+    (is (= -0x80 (first (padding-bytes "ab"))))))
 
 (deftest test-padding-longer-msg
   ; 520 bits, so wraps around
-  (let [msg (apply str (take 65 (repeat "a")))
-        mb (msg->bytes msg)]
-    (is (= 55 (count (padding-bytes mb))))))
+  (testing "pads message longer than 512 bits to be congruent with 448
+            mod 512, leaving 8 bytes for message length in next block"
+    (let [msg (apply str (take 65 (repeat "a")))
+          mb (msg->bytes msg)]
+      (is (= 55 (count (padding-bytes mb))))
+      (is (= -0x80 (first (padding-bytes mb)))))))
 
 (deftest test-padding-448-512-msg
-  (let [msg (apply str (take 57 (repeat "a")))
-        mb (msg->bytes msg)]
-    (is (= 63 (count (padding-bytes mb))))))
+  (testing "message longer that is greater than 448 mod 512 bits still has to
+            pad to 448 congruence in next block since there aren't enough bits
+            left in current block for 64-bit message-length encoding"
+    (let [msg (apply str (take 57 (repeat "a")))
+          mb (msg->bytes msg)]
+      (is (= 63 (count (padding-bytes mb))))
+      (is (= "10000000" (bstring (first (padding-bytes mb)))))
+      (is (= -0x80 (first (padding-bytes mb)))))))
 
 (deftest test-msg-length-bytes
-  (is (= 8 (count (msg-length-bytes "a"))))
-  (is (= (map bstring (byte-array (concat (take 7 (repeat 0)) [1])))
-         (map bstring (msg-length-bytes "a")))))
+  (testing "generates byte-array for encoding message length as 64-bit long"
+    (is (= 8 (count (msg-length-bytes "a"))))
+    (is (= (map bstring (byte-array (concat (take 7 (repeat 0)) [1])))
+           (map bstring (msg-length-bytes "a"))))))
+
+(deftest test-prepping-msg-as-byte-array
+  (testing "preps message by converting to bytes, adding padding separator bit,
+            adding congruence padding, and adding message length bits"
+    (let [m "ab"
+          prepped (msg->prepped-barray "ab")]
+      (is (= 64 (count prepped)))
+      (is (= (int \a) (aget prepped 0)))
+      (is (= (int \b) (aget prepped 1)))
+      (is (= 2 (last prepped)))
+      (is (= -0x80 (aget prepped 2))))))
